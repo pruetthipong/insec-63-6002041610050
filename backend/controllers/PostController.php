14ae76fdd5c448cb9175dbced0f0259f184633aa
Pruetthipong <fernfernzaa16@hotmail.com>
2020-09-24 18:35:04 +0700

    'ABAC'

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -8,6 +8,7 @@ use backend\models\PostSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * PostController implements the CRUD actions for Post model.
@@ -19,7 +20,18 @@ class PostController extends Controller
      */
     public function behaviors()
     {
-        return [
+        return [ 
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['index', 'create', 'delete','update'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
