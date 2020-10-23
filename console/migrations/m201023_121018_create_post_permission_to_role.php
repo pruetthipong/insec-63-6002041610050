2a09937c7b43904ae0463e631080b3586af77133
Pruetthipong Rodrang <fernfernzaa16@hotmail.com>
2020-10-23 19:17:36 +0700

    Update create migration for asssign parent and child new

--- console/migrations/m201023_121018_create_post_permission_to_role.php
+++ console/migrations/m201023_121018_create_post_permission_to_role.php
@@ -3,41 +3,40 @@
 use yii\db\Migration;
 
 /**
- * Class m201022_113422_create_post_permission_to_role
+ * Class m201023_121018_create_post_permission_to_role
  */
-class m201022_113422_create_post_permission_to_role extends Migration
+class m201023_121018_create_post_permission_to_role extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
+
         $auth = Yii::$app->authManager;
-        
+
         // get role
         $author = $auth->getRole('author');
         $admin = $auth->getRole('admin');
         $superAdmin = $auth->getRole('super-admin');
 
-        //get Permission
+        // get permission
         $listPost = $auth->getPermission('post-index');
         $createPost = $auth->getPermission('post-create');
-        $deletePost = $auth->getPermission('post-delete');
         $updatePost = $auth->getPermission('post-update');
         $viewPost = $auth->getPermission('post-view');
+        $deletePost = $auth->getPermission('post-delete');
 
-        //assign
-        $auth->addChild($author,$createPost);
-        $auth->addChild($author,$listPost);
-        $auth->addChild($author,$viewPost);
-        $auth->addChild($author,$updatePost);
+        // assign
+        $auth->addChild($author, $createPost);
+        $auth->addChild($author, $listPost);
+        $auth->addChild($author, $viewPost);
+        $auth->addChild($author, $updatePost);
 
-        $auth->addChild($admin,$author);
+        $auth->addChild($admin, $author);
 
-        $auth->addChild($superAdmin,$admin);
-        $auth->addChild($superAdmin,$deletePost);
-
-
+        $auth->addChild($superAdmin, $admin);
+        $auth->addChild($superAdmin, $deletePost);
     }
 
     /**
@@ -45,7 +44,7 @@ class m201022_113422_create_post_permission_to_role extends Migration
      */
     public function safeDown()
     {
-        
+
         $auth = Yii::$app->authManager;
 
          // get role
@@ -70,10 +69,9 @@ class m201022_113422_create_post_permission_to_role extends Migration
  
          $auth->removeChild($superAdmin,$admin);
          $auth->removeChild($superAdmin,$deletePost);
+
         
-
-
-        //echo "m201022_113422_create_post_permission_to_role cannot be reverted.\n";
+       // echo "m201023_121018_create_post_permission_to_role cannot be reverted.\n";
 
         return true;
     }
@@ -87,7 +85,7 @@ class m201022_113422_create_post_permission_to_role extends Migration
 
     public function down()
     {
-        echo "m201022_113422_create_post_permission_to_role cannot be reverted.\n";
+        echo "m201023_121018_create_post_permission_to_role cannot be reverted.\n";
 
         return false;
     }
